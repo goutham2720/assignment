ba1bfc98cf273333e6b061863a8722530223b370
goutham-krishan <gauthamchangamannil@gamil.com>
2022-10-25 10:45:25 +0530

    added welcome greating

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ function buttonClick(number) {
 function clearScreen() {
   document.getElementById("screen").value = "";
 }
+
 function enter() {
   let valueOnScreen = document.getElementById("screen").value;
   let password = "1234";
   if (valueOnScreen == password) {
     // document.getElementById("errorLabel").style.display = "none";
     document.getElementById("container").style.display = "none";
+    document.getElementById("greatiText").style.display = "block";
   } else {
     document.getElementById("errorLabel").style.display = "block";
     document.getElementById("container").style.boxShadow =
       " 0px 0px 5px 2px red";
   }
-//   console.log(valueOnScreen);
+  //   console.log(valueOnScreen);
 }
